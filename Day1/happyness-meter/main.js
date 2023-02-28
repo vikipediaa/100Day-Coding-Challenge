1144eaaf9cabc14d3524f7ade40e60b4ae95b619
Vikas Pandey <pandey222vikas@gmail.com>
2023-02-28 19:58:23 +0530

    done

--- Day1/happyness-meter/main.js
+++ Day1/happyness-meter/main.js
@@ -1,29 +1,27 @@
-var input1 = document.querySelector(".mon-feb");
-var input2 = document.querySelector(".mon-march");
-var input3 = document.querySelector(".mon-april");
-var input4 = document.querySelector(".mon-may");
-var td1=input1.getElementsByClassName("td-data");
-// console.log(input1.innerHTML="hello world");
-console.log(td1);
+var input2 = document.querySelector(".mon-feb");
+var input3 = document.querySelector(".mon-march");
+var input4 = document.querySelector(".mon-april");
+var input5 = document.querySelector(".mon-may");
+var input6 = document.querySelector(".mon-june");
+var input7 = document.querySelector(".mon-july");
+var input8 = document.querySelector(".mon-aug");
+var input9 = document.querySelector(".mon-sep");
+var input10 = document.querySelector(".mon-oct");
+var input11 = document.querySelector(".mon-nov");
+var input12 = document.querySelector(".mon-dec");
+var td1=document.getElementsByClassName("td-data");
+var year=document.getElementById("year");
 
-// input1.innerHTML="hello world";
-// td_dataChange[1].style.backgroundcolor="red";
-// td_dataChange[1].innerHTML="hello";
-// console.log(td_dataChange[2].innerHTML);
-// let btn = document.getElements(".month table tr td");
-// console.log(btn);
-// var header = document.querySelector(".calendra.month");
-// console.log(header.innerHTML);
 
-function myFunction() {
- td_dataChange[2].style.borderColor="red";
-}
+// console.log(td1);
+
+
 function createCalendar(elem, year, month) {
 
     let mon = month - 1; // months in JS are 0..11, not 1..12
     let d = new Date(year, mon);
 
-    let table = '<div class="month"><table><tr><th>MO</th><th>TU</th><th>WE</th><th>TH</th><th>FR</th><th>SA</th><th>SU</th></tr><tr></div>';
+    let table = '<div class="month"><div id="year">2023</div><table><tr><th>MO</th><th>TU</th><th>WE</th><th>TH</th><th>FR</th><th>SA</th><th>SU</th></tr><tr></div>';
 
     // spaces for the first row
     // from Monday till the first day of the month
@@ -63,36 +61,40 @@ function createCalendar(elem, year, month) {
   }
 //   header.style.margin="1rem";
 // createCalendar(input1, 2012, 9);
-createCalendar(input1,2023,2);
-createCalendar(input2,2023,3);
-createCalendar(input3,2023,4);
-createCalendar(input4,2023,5);
-// btn.addEventListener("onClick",changeBackgoungColor);
-// for(var i=0; i<td_dataChange.length; i++)
-// {
-  // td_dataChange[2].addEventListener("mouseover",myFunction);
-// }
-// td_dataChange.addEventListener("click",myFunction);
-// console.log(td1.innerHTML);
-// console.log(td1.length)
-for(var i=0; i<=td1.length; i++){
-// td1[i].style.backgroundColor="red";
+createCalendar(input2,2023,2);
+createCalendar(input3,2023,3);
+createCalendar(input4,2023,4);
+createCalendar(input5,2023,5);
+createCalendar(input6,2023,6);
+createCalendar(input7,2023,7);
+createCalendar(input8,2023,8);
+createCalendar(input9,2023,9);
+createCalendar(input10,2023,10);
+createCalendar(input11,2023,11);
+createCalendar(input12,2023,12);
+console.log(year);
+
+
+{
+let score=[];
+for(let i=0;i<td1.length;i++)
+{
+  score[i]=td1[i];
 }
+for(let i=0;i<td1.length;i++)
+{
 
+  td1[i].addEventListener("click",()=>{
+    score[i].style.backgroundColor="#00a82d";
+    // score[1].style.backgroundColor="red";
+  })
+}
+for(let i=0;i<td1.length;i++)
+{
 
-// console.log(input.innerHTML);
-// function changeBackgroundColor(){
-// td1.style.backgroundcolor="red";
-// }
-// td1[6].style.backgroundColor="green";
-for(var i=0; i<=td1.length; i++){
-  td1[i].addEventListener("click",beingHappy,false);
-  }
-  function beingHappy(e)
-  {
-   if(e.target==e.currentTarget)
-   {
-      td1[e].style.backgroundColor="green";
-   } 
-  }
-// td1[6].addEventListener("click",beingHappy);
+  td1[i].addEventListener("dblclick",()=>{
+    // score[i].style.backgroundColor="green";
+    score[i].style.backgroundColor="red";
+  })
+}
+}
